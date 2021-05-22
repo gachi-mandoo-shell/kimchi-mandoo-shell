8e3cc7f3699fb379c6bd969c017ebf19b049ade7
JaeSeoKim <devjaeseo@gmail.com>
2021-05-22 22:42:30 +0900

    Update ft_cmd_bonus.c

--- src_bonus/exec/ft_cmd_bonus.c
+++ src_bonus/exec/ft_cmd_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "minishell_bonus.h"
 
 int	ft_cmd_exec(t_cmd *cmds, t_check *g)
 {
